2ffc4db861ed93402e1d1891005a1e5dbda6a555
pangyuteng <pteng@mednet.ucla.edu>
2022-05-27 10:45:14 -0700

    updated oom oneliner

--- oom.sh
+++ oom.sh
@@ -2,6 +2,6 @@
 
 echo $@
 
-python3 -c "import numpy as np;x=2048;a=np.random.rand(x,x,x);print(a.shape)"
+python3 -c "import numpy as np;x=1024*3;a=np.random.rand(x,x,x);print(a.shape)"
 
 # ^^^ allocate 8bytes per float64, 8 * ( 2048 ^ 3) = 68.7 GB
